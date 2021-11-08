17a0fcf8a1d8b5152d64b11e24f532357ca2cc32
PlayerG9 <g9yout@gmail.com>
2021-11-08 19:49:55 +0100

    added linux support (only showinfo, showwarning, showerror, askquestion)

--- src/messagebox/_linux.py
+++ src/messagebox/_linux.py
@@ -1,23 +1,39 @@
 # -*- coding=utf-8 -*-
 r"""
-
+possible option is /usr/bin/notify-send
 """
+import subprocess
+
+
+MB_INFO = '--info'
+MB_WARNING = '--warning'
+MB_ERROR = '--error'
+
+
+def _msg(title: str, message: str, what):
+    result = subprocess.run([
+        '/usr/bin/zenity',
+        what,
+        '--title', title,
+        '--text', message
+    ])
+    return result.returncode
 
 
 def showinfo(title: str, message: str):
-    pass
+    _msg(title, message, MB_INFO)
 
 
 def showwarning(title: str, message: str):
-    pass
+    _msg(title, message, MB_WARNING)
 
 
 def showerror(title: str, message: str):
-    pass
+    _msg(title, message, MB_ERROR)
 
 
 def askquestion(title: str, message: str):
-    pass
+    print(_msg(title, message, MB_INFO))
 
 
 def askokcancel(title: str, message: str):
@@ -25,7 +41,7 @@ def askokcancel(title: str, message: str):
 
 
 def askyesno(title: str, message: str):
-    pass
+    _msg(title, message, MB_INFO)
 
 
 def askyesnocancel(title: str, message: str):
