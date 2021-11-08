ad529cfd1282bbc1461d83ccea0dc041a377aa6d
PlayerG9 <g9yout@gmail.com>
2021-11-08 18:41:29 +0100

    declare functions

--- src/messagebox/_dummy.py
+++ src/messagebox/_dummy.py
@@ -2,3 +2,70 @@
 r"""
 
 """
+
+__all__ = [
+    "showinfo",
+    "showwarning",
+    "showerror",
+    "askquestion",
+    "askokcancel",
+    "askyesno",
+    "askyesnocancel",
+    "askretrycancel"
+]
+
+
+def showinfo(title: str, message: str):
+    r"""
+    test
+    """
+    pass
+
+
+def showwarning(title: str, message: str):
+    r"""
+
+    """
+    pass
+
+
+def showerror(title: str, message: str):
+    r"""
+
+    """
+    pass
+
+
+def askquestion(title: str, message: str):
+    r"""
+
+    """
+    pass
+
+
+def askokcancel(title: str, message: str):
+    r"""
+
+    """
+    pass
+
+
+def askyesno(title: str, message: str):
+    r"""
+
+    """
+    pass
+
+
+def askyesnocancel(title: str, message: str):
+    r"""
+
+    """
+    pass
+
+
+def askretrycancel(title: str, message: str):
+    r"""
+
+    """
+    pass
