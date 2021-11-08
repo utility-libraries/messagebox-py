915954268816123b3122ccb3f6df8246a96fc11b
PlayerG9 <g9yout@gmail.com>
2021-11-08 19:28:37 +0100

    added template for new platform-support

---
+++ docs/newplatform.py
@@ -0,0 +1,36 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+
+
+def showinfo(title: str, message: str):
+    pass
+
+
+def showwarning(title: str, message: str):
+    pass
+
+
+def showerror(title: str, message: str):
+    pass
+
+
+def askquestion(title: str, message: str):
+    pass
+
+
+def askokcancel(title: str, message: str):
+    pass
+
+
+def askyesno(title: str, message: str):
+    pass
+
+
+def askyesnocancel(title: str, message: str):
+    pass
+
+
+def askretrycancel(title: str, message: str):
+    pass
